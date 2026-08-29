5209e3c9eff7dabc637b9d2d5858a8b801f31857
Ashifa parween <ashifaparween43@gmail.com>
2026-08-29 20:35:27 +0530

    Time: 0 ms (100%), Space: 42.9 MB (12.47%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+        int count=0;
+        int score=0;
+        for(int i=0;i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                count++;
+            }
+            else{
+                count--;
+                if(s.charAt(i-1)=='('){
+                     score+=1<<count;
+
+
+                }
+               
+            }
+        }
+        return score;
+    }
+}
